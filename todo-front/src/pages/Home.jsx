c2af4c222c7e7e4102587c595640e22761f1c5f6
berchiche willem <berwillempro@gmail.com>
2024-10-03 10:27:45 +0100

    added main section

--- todo-front/src/pages/Home.jsx
+++ todo-front/src/pages/Home.jsx
@@ -1,4 +1,11 @@
 import "../styles/Home.css";
+import mainIcon from "./../assets/images/mainIcon.gif";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="intro">
+        <img src={mainIcon} alt="main-icon" className="mainIcon" />
+      </div>
+    </div>
+  );
 }
