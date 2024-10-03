d392bb85fadc3131844b71cc8e5fe588b5e5ba2b
Razmokette2 <razinemebarek@gmail.com>
2024-10-03 12:12:23 +0100

    first commit

--- todo-front/src/main.jsx
+++ todo-front/src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayouut from "./layouts/MainLayout/MainLayouut";
+import MainLayout from "./layouts/MainLayout/MainLayouut"; // Corrected spelling
 import Home from "./pages/Home";
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayouut></MainLayouut>,
+    element: <MainLayout></MainLayout>,
     children: [
       {
         path: "home",
@@ -16,7 +17,18 @@ const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: "/register",
+    element: <MainLayout></MainLayout>, // Added missing comma
+    children: [
+      {
+        path: "",
+        element: <Register></Register>, // Empty path because it's the same as the parent
+      },
+    ],
+  },
 ]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
